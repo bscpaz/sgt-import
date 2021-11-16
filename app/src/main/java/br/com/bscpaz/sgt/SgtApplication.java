8566c509ff6d2bdbffc6f6c325536c4822ab0da2
Bruno Paz <soujava@gmail.com>
2021-11-16 10:05:08 -0300

    Adding XLSX reader

--- app/src/main/java/br/com/bscpaz/sgt/SgtApplication.java
+++ app/src/main/java/br/com/bscpaz/sgt/SgtApplication.java
@@ -1,7 +1,15 @@
 package br.com.bscpaz.sgt;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+
 import javax.annotation.PostConstruct;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,8 +21,38 @@ public class SgtApplication {
 	}
 
 	@PostConstruct
-	public static void importFile() {
-		System.out.println("hello!");
+	public static void importFile() throws IOException {
+		readExcel();
 	}
 
+	private static void readExcel() throws IOException {
+		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+		InputStream fis = classloader.getResourceAsStream("assuntos.xlsx");
+		
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet = wb.getSheetAt(0);
+		
+		Iterator<Row> rowIterator = sheet.iterator(); 
+		
+		while (rowIterator.hasNext()) { 
+			Row row = rowIterator.next(); 
+			Iterator<Cell> cellIterator = row.cellIterator(); 
+
+			while (cellIterator.hasNext()) { 
+				Cell cell = cellIterator.next(); 
+				switch (cell.getCellType()) { 
+					case STRING: 
+						System.out.print(cell.getStringCellValue() + "\t"); 
+						break; 
+					case NUMERIC: 
+						System.out.print(cell.getNumericCellValue() + "\t"); 
+						break; 
+					case BOOLEAN: 
+						System.out.print(cell.getBooleanCellValue() + "\t"); 
+						break; default : 
+					} 
+				} 
+			System.out.println("");
+		}
+	}
 }
